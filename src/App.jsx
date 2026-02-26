e2e47f09de7098befcacd8d16038cb709983d338
AnkitKumarMishra5 <ankit5kumar14@gmail.com>
2026-02-26 22:02:23 +0530

    Users: Update, Delete

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css'
 
@@ -7,10 +9,57 @@ import Users from './components/Users'
 import AddUser from './components/AddUser'
 
 function App() {
+  const [users, setUsers] = useState([])
+  const [editMode, setEditMode] = useState(false)
+  const [editUser, setEditUser] = useState(null)
+  const API_URL = import.meta.env.VITE_API_URL
+
+  useEffect(() => {
+    fetchUsers()
+  }, [])
+
+  useEffect(() => {
+    setEditMode(Boolean(editUser))
+  }, [editUser])
+
+  function resetToNormalMode() {
+    setEditMode(false)
+    setEditUser(null)
+  }
+  
+  async function fetchUsers() {
+    try {
+      const res = await axios.get(`${API_URL}/users`)
+      setUsers(res.data.data)
+    } catch(error) {
+      console.log('Error while fetching users', error)
+    }
+  }
+
+  async function deleteUser(userId) {
+    try {
+      const res = await axios.delete(`${API_URL}/users/${userId}`)
+      alert(res.data.message)
+      fetchUsers()
+    } catch(error) {
+      alert('Error while deleting user')
+      console.log('Error while deleting user', error)
+    }
+  }
+
   return (
     <Container className='mt-5'>
-      <Users />
-      <AddUser />
+      <Users 
+        users={users} 
+        setEditUser={setEditUser} 
+        deleteUser={deleteUser}
+      />
+      <AddUser 
+        fetchUsers={fetchUsers} 
+        editMode={editMode} 
+        editUser={editUser}
+        resetToNormalMode={resetToNormalMode}
+      />
     </Container>
   )
 }
--- src/components/AddUser.jsx
+++ src/components/AddUser.jsx
@@ -1,13 +1,26 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import axios from 'axios'
 
-function AddUser() {
+function AddUser({ fetchUsers, editMode, editUser, resetToNormalMode }) {
   const [name, setName] = useState('')
   const [email, setEmail] = useState('')
   const [age, setAge] = useState('')
   const API_URL = import.meta.env.VITE_API_URL
+
+  useEffect(() => {
+    if(editUser) {
+      setName(editUser.name)
+      setAge(editUser.age)
+    }
+  }, [editUser])
+
+  function reset() {
+    setName('')
+    setEmail('')
+    setAge('')
+  }
 
   async function addUser() {
     try {
@@ -18,14 +31,36 @@ function AddUser() {
       }
       const res = await axios.post(`${API_URL}/users`, newUser)
       alert(res.data.message)
+      reset()
+      fetchUsers()
     } catch(error) {
+      alert('Error while creating user')
       console.log('Error while creating user', error)
+    }
+  }
+
+  async function updateUser() {
+    try {
+      const newUser = {
+        name,
+        age
+      }
+      const res = await axios.patch(`${API_URL}/users/${editUser._id}`, newUser)
+      alert(res.data.message)
+      reset()
+      fetchUsers()
+      resetToNormalMode()
+    } catch(error) {
+      alert('Error while updating user')
+      console.log('Error while updating user', error)
     }
   }
 
   return (
     <div className='mt-5'>
-      <h1 className='display-6'>Add User</h1>
+      <h1 className='display-6'>
+        { !editMode ? 'Add' : 'Edit'} User
+      </h1>
       <Form>
         <Form.Group className="mb-3">
           <Form.Label>Name</Form.Label>
@@ -34,13 +69,15 @@ function AddUser() {
             onChange={(e) => setName(e.target.value)}
           />
         </Form.Group>
-        <Form.Group className="mb-3">
-          <Form.Label>Email</Form.Label>
-          <Form.Control type="email" placeholder="Enter your email"
-            value={email} 
-            onChange={(e) => setEmail(e.target.value)}
-          />
-        </Form.Group>
+         { !editMode && (
+          <Form.Group className="mb-3">
+            <Form.Label>Email</Form.Label>
+            <Form.Control type="email" placeholder="Enter your email"
+              value={email} 
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </Form.Group>
+        )}
         <Form.Group className="mb-3">
           <Form.Label>Age</Form.Label>
           <Form.Control type="number" placeholder="Enter your age"
@@ -48,8 +85,10 @@ function AddUser() {
             onChange={(e) => setAge(e.target.value)}
           />
         </Form.Group>
-        <Button variant="dark" type="button" onClick={addUser}>
-          Add
+        <Button variant="dark" type="button" 
+          onClick={() => !editMode ? addUser() : updateUser()}
+        >
+          { !editMode ? 'Add' : 'Update' }
         </Button>
       </Form>
     </div>
